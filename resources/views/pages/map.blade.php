030577452f626fe17b6a830b745a8e189bd3ce9b
Ichsan Munadi <ichsan.dgn@gmail.com>
2020-09-25 17:00:31 +0700

    direction maps

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -39,56 +39,25 @@
                     </a>
                 </div> -->
                 <div id="floating-panel" class="invisible" data-toggle="appear" data-class="animated fadeInUp" data-timeout="300">
-                <a class="btn btn-hero btn-sm btn-alt-success mb-10 mx-5" >
-                        <i class="fa fa-fw fa-map-marker mr-1"></i> My Location
-                    </a>
-                    <b>Start: </b>
-                    <select id="start">
-                        <option value="chicago, il">Chicago</option>
-                        <option value="st louis, mo">St Louis</option>
-                        <option value="joplin, mo">Joplin, MO</option>
-                        <option value="oklahoma city, ok">Oklahoma City</option>
-                        <option value="amarillo, tx">Amarillo</option>
-                        <option value="gallup, nm">Gallup, NM</option>
-                        <option value="flagstaff, az">Flagstaff, AZ</option>
-                        <option value="winona, az">Winona</option>
-                        <option value="kingman, az">Kingman</option>
-                        <option value="barstow, ca">Barstow</option>
-                        <option value="san bernardino, ca">San Bernardino</option>
-                        <option value="los angeles, ca">Los Angeles</option>
-                    </select>
-                    <b>End: </b>
-                    <select id="end">
-                        <option value="chicago, il">Chicago</option>
-                        <option value="st louis, mo">St Louis</option>
-                        <option value="joplin, mo">Joplin, MO</option>
-                        <option value="oklahoma city, ok">Oklahoma City</option>
-                        <option value="amarillo, tx">Amarillo</option>
-                        <option value="gallup, nm">Gallup, NM</option>
-                        <option value="flagstaff, az">Flagstaff, AZ</option>
-                        <option value="winona, az">Winona</option>
-                        <option value="kingman, az">Kingman</option>
-                        <option value="barstow, ca">Barstow</option>
-                        <option value="san bernardino, ca">San Bernardino</option>
-                        <option value="los angeles, ca">Los Angeles</option>
+                    <b>To: </b>
+                    <select id="to">
+                        @foreach($data->locations as $locations)
+                        <option value="{{$locations->locations_id}}" lat="{{$locations->locations_latitude}}" lng="{{$locations->locations_longitude}}">{{$locations->locations_name}}</option>
+                        @endforeach
+
                     </select>
                     <b>Mode: </b>
-                    <select id="end">
-                        <option value="chicago, il">Chicago</option>
-                        <option value="st louis, mo">St Louis</option>
-                        <option value="joplin, mo">Joplin, MO</option>
-                        <option value="oklahoma city, ok">Oklahoma City</option>
-                        <option value="amarillo, tx">Amarillo</option>
-                        <option value="gallup, nm">Gallup, NM</option>
-                        <option value="flagstaff, az">Flagstaff, AZ</option>
-                        <option value="winona, az">Winona</option>
-                        <option value="kingman, az">Kingman</option>
-                        <option value="barstow, ca">Barstow</option>
-                        <option value="san bernardino, ca">San Bernardino</option>
-                        <option value="los angeles, ca">Los Angeles</option>
+                    <select id="mode">
+                        <option value="DRIVING">DRIVING</option>
+                        <option value="BICYCLING">BICYCLING</option>
+                        <option value="TRANSIT">TRANSIT</option>
+                        <option value="WALKING">WALKING</option>
                     </select>
-                    <a class="btn btn-hero btn-sm btn-alt-primary mb-10 mx-5" >
+                    <a class="btn btn-hero btn-sm btn-alt-primary mb-10 mx-5" onclick="direction()">
                         <i class="fa fa-fw fa-search mr-1"></i> Direction
+                    </a>
+                    <a class="btn btn-hero btn-sm btn-alt-success mb-10 mx-5" onclick="init('sad')">
+                        <i class="fa fa-fw fa-map-marker mr-1"></i> My Location
                     </a>
                 </div>
                 <div id="Map" style="height:500px" class="invisible" data-toggle="appear" data-class="animated fadeInUp" data-timeout="300"></div>
@@ -109,15 +78,24 @@
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBF9mRyaqTj12UxNUJmHFfZWbrCp_APwrM"></script>
 <script>
     var locations = [
-        @foreach($data->locations as $mylocations)
-            ["{{$mylocations->locations_name}}", "{{$mylocations->locations_latitude}}", "{{$mylocations->locations_longitude}}", "{{$mylocations->locations_address}}", "{{$mylocations->locations_id}}"],
+        @foreach($data -> locations as $mylocations)["{{$mylocations->locations_name}}", {{$mylocations -> locations_latitude}}, {{$mylocations -> locations_longitude}}, "{{$mylocations->locations_address}}", "{{$mylocations->locations_id}}"],
         @endforeach
     ];
-    
-    const markerArray = [];
-    var currentlocation ;
+    const image = {
+        url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        size: new google.maps.Size(20, 32),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 32),
+    };
+
+    var icon = {
+        url: "https://www.waroengss.com/filemanager/image/icon-map.png"
+    };
+    var markers = [];
+    var markerArray = [];
+    var currentlocation;
     const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer();
+    var directionsRenderer = new google.maps.DirectionsRenderer();
     var map = new google.maps.Map(document.getElementById('Map'), {
         zoom: 15,
         center: new google.maps.LatLng(-2.0188205365653173, 107.96703549708104),
@@ -125,76 +103,92 @@
     });
     directionsRenderer.setMap(map);
 
-    const onChangeHandler = function () {
+    function init(set = null) {
+        markers = [];
+        markerArray = [];
+        currentlocation;
+        if (set) {
+            directionsRenderer = new google.maps.DirectionsRenderer();
+            map = new google.maps.Map(document.getElementById('Map'), {
+                zoom: 15,
+                center: new google.maps.LatLng(-2.0188205365653173, 107.96703549708104),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+            directionsRenderer.setMap(map);
+            
+        }
+
+
+        // directionsRenderer.setDirections(null);
+
+        // const onChangeHandler = function () {
+        //     calculateAndDisplayRoute(directionsService, directionsRenderer);
+        // };
+
+        // document.getElementById("start").addEventListener("change", onChangeHandler);
+        // document.getElementById("end").addEventListener("change", onChangeHandler);
+        var infowindow = new google.maps.InfoWindow();
+
+
+        mylocation();
+        for (i = 0; i < locations.length; i++) {
+            marker = new google.maps.Marker({
+                // icon: icon,
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map
+            });
+            markers.push(marker);
+
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent('<div style="width:200px; height:auto;" id="content">' + '<div id="siteNotice">' +
+                        '</div>' +
+                        '<h2 style="font-size:20px" id="firstHeading" class="firstHeading">' + locations[i][0] + '</h2>' +
+                        '<div id="bodyContent">' +
+                        '<p>' + locations[i][3] + '<br><br><a style="text-center" onclick="outletdetail(' + locations[i][4] + ')" id="maps-btn">Detail</a></p>' +
+                        '<p></p></div>' +
+                        '</div>');
+
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+
+        }
+    }
+
+    function direction() {
         calculateAndDisplayRoute(directionsService, directionsRenderer);
-    };
-
-    document.getElementById("start").addEventListener("change", onChangeHandler);
-    document.getElementById("end").addEventListener("change", onChangeHandler);
-    var infowindow = new google.maps.InfoWindow();
-    const image = {
-        url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-        size: new google.maps.Size(20, 32),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(0, 32),
-    };
-    
-    var icon = {
-        url: "https://www.waroengss.com/filemanager/image/icon-map.png"
-    };
-    
-    mylocation();
-    for (i = 0; i < locations.length; i++) {
-        marker = new google.maps.Marker({
-            // icon: icon,
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            map: map
-        });
-
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infowindow.setContent('<div style="width:200px; height:auto;" id="content">' + '<div id="siteNotice">' +
-                    '</div>' +
-                    '<h2 style="font-size:20px" id="firstHeading" class="firstHeading">' + locations[i][0] + '</h2>' +
-                    '<div id="bodyContent">' +
-                    '<p>' + locations[i][3] + '<br><br><a style="text-center" onclick="outletdetail(' + locations[i][4] + ')" id="maps-btn">Detail</a></p>' +
-                    '<p></p></div>' +
-                    '</div>');
-
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
-
     }
 
     function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-        directionsService.route(
-            {
-            origin: currentlocation,
-            destination: {
-                lat: -7.755874,
-                lng: 110.376575
-            },
-            
-            // destination: {
-            //     query: document.getElementById("end").value,
-            // },
-            travelMode: google.maps.TravelMode.DRIVING,
+        let des = {
+            lat: null,
+            lng: null
+        };
+        locations.forEach(loc => {
+            (loc[4] == document.getElementById("to").value) ? des.lat = loc[1]: null;
+            (loc[4] == document.getElementById("to").value) ? des.lng = loc[2]: null;
+        })
+        directionsService.route({
+                origin: currentlocation,
+                destination: des,
+                travelMode: document.getElementById("mode").value,
             },
             (response, status) => {
                 console.log('ini response', response);
                 console.log('ini currentlocation', currentlocation);
-                console.log('ini start', document.getElementById("start").value);
-                console.log('ini end', document.getElementById("end").value);
-                console.log('ini travelMode', google.maps.TravelMode.DRIVING);
-            if (status === "OK") {
-                directionsRenderer.setDirections(response);
-            } else {
-                window.alert("Directions request failed due to " + status);
-            }
+                // console.log('ini lat', parseInt(document.getElementById("to").lat));
+                console.log('ini des', des);
+                console.log('ini travelMode', document.getElementById("mode").value);
+                if (status === "OK") {
+                    directionsRenderer.setDirections(response);
+                } else {
+                    window.alert("Directions request failed due to " + status);
+                }
             }
         );
-        }
+    }
+
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(
@@ -205,11 +199,11 @@
         infoWindow.open(map);
     }
 
-    function mylocation(){
+    function mylocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
-                    console.log('position', position );
+                    console.log('position', position);
                     const pos = {
                         lat: position.coords.latitude,
                         lng: position.coords.longitude,
@@ -219,7 +213,7 @@
                         icon: image,
                         position: new google.maps.LatLng(pos.lat, pos.lng),
                         map: map,
-                        draggable: true
+                        draggable: false
                     });
                     console.log('ini pos', pos);
                     map.setCenter(pos);
@@ -232,5 +226,6 @@
             handleLocationError(false, infoWindow, map.getCenter());
         }
     }
+    init();
 </script>
 @endsection
